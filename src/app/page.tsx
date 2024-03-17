8a2b034bcaf3daeb57b5f02fedf4e45a57234ef3
Mohsin <mdmohsinhaider6@gmail.com>
2024-03-17 16:51:47 +0530

    remove them button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Modetogglebutton from "@/components/Modetogglebutton";
 import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
       <Button className=" bg-blue-700 hover:bg-red-700 m-2">Click </Button>
-      <Modetogglebutton />
     </div>
   );
 }
