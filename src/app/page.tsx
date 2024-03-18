629a34c3205a86cbb806461e472f8d195449919a
Mohsin <mdmohsinhaider6@gmail.com>
2024-03-19 01:40:05 +0530

    Baker card show pis added comonents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Thembuttontoogle from "@/components/Thembuttontoogle";
 import { Button } from "@/components/ui/button"
 import Herosection from "@/components/Herosection";
+import Bakeryshowpish from "@/components/Bakershowpishcard";
 export default function Home() {
   return (
     <main>
       <Herosection />
+      <Bakeryshowpish />
     </main>
   );
 }
